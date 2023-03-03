c640cb09848630b279c480f4a37d1b4cf78c882b
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-03-03 14:01:01 +0600

    sda
    sda

--- src/pages/app-backend/Payment-success.jsx
+++ src/pages/app-backend/Payment-success.jsx
@@ -23,12 +23,15 @@ const PaymentSuccess = () => {
   
 
     const secretKey = process.env.REACT_APP_PAYMENT_PIN
+    const ciphertext = key.replace(/p1L2u3S/g, '+' ).replace(/s1L2a3S4h/g, '/').replace(/e1Q2u3A4l/g, '=');
+
+    const bytes = AES.decrypt(ciphertext, secretKey);
+        const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+        
+
     const  PaymentProcess = (id) => {
       const  key='consumer_key=ck_7d700d7c05bea9f024076feb890944ad286703f2&consumer_secret=cs_59a8c6db54711f8a9fc314b95e0ad782a946c191'
     const    bodys = `{"status": "processing", "payment_method": "SSLCOMMERZ"}`
-
-
-
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
   var requestOptions = {
@@ -41,26 +44,18 @@ const PaymentSuccess = () => {
     .then(response => response.json())
     .then(result => {
       const rslt = result;
-      console.log(rslt) })
+    //   console.log(rslt) 
+    navigate(`/order/${decryptedData}`) 
+    })
 
     }
     
 
 
     useEffect(() => {
-        // const secretKey = 'mysecretkey';
-        // const encryptedData = 'U2FsdGVkX1/9eMYb8nmg3rU6ekwFnPhn5U6M8mzvB7M='
-        // const testKey = 'U2FsdGVkX19HJ34mNYLt9b2hztCEp1L2u3SnSp1L2u3Stqoq3UrQ8Tke1Q2u3A4l'
-        const ciphertext = key.replace(/p1L2u3S/g, '+' ).replace(/s1L2a3S4h/g, '/').replace(/e1Q2u3A4l/g, '=');
-        // console.log(ciphertext)
-
-        
-        const bytes = AES.decrypt(ciphertext, secretKey);
-        const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
-        
-        // console.log( decryptedData);
+      
         PaymentProcess(decryptedData)
-        navigate(`/order/${decryptedData}`) 
+       
       
     }, [])
   return (
